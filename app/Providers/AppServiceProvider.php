ce50f6d8365ae774316ff705d4c2a00e5846ed1a
Fernando Sandoval <sandovalf193@gmail.com>
2021-05-02 20:06:23 -0600

    procfile added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,4 +16,15 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+    }
+
 }
